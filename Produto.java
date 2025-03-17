7e929103904d8ad11f13b7a0ef5507f4a6ae78cb
TayCardoso25 <taynara75.cardoso@gmail.com>
2025-03-17 16:49:07 -0300

    Teste do tio

--- Produto.java
+++ Produto.java
@@ -1,4 +1,5 @@
 public class Produto {
+    //Teste
     private String descricao;
     private int quantidade;
     private double valorUnitario;
--- testeProduto.java
+++ testeProduto.java
@@ -1,5 +1,6 @@
 public class testeProduto {
     public static void main(String[] args) {
+        //Teste
       
         
             Produto produto = new Produto("Bisnaguinha", 20, 0.40,  'P',true);
